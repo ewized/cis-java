3674da7b5eef1e72d8e7027492753a4d79fec906
Joshua Rodriguez <ewized@gmail.com>
2014-05-05 11:55:34 -0700

    Add face program

---
+++ face/Face.java
@@ -0,0 +1,19 @@
+/*
+    Filename: Face.java
+    Description: Make a face.
+    Author: Joshua Rodriguez
+    Email: ewized@gmail.com
+    Date: 02/10/2014
+*/
+public class Face {
+    public static void main(String[] args) {
+        System.out.println("   \1\1\1\1\1\1\1\1");
+        System.out.println("  \1         \1");
+        System.out.println(" \1    \1  \1    \1");
+        System.out.println("\1      \1       \1");
+        System.out.println(" \1            \1");
+        System.out.println("  \1   \1\1\1\1   \1");
+        System.out.println("   \1        \1");
+        System.out.println("    \1\1\1\1\1\1\1\1");
+    }
+} 
