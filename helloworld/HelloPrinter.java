92c13b5a5469b9b00ae6e54f50348f772d4c229c
Joshua Rodriguez <ewized@gmail.com>
2014-05-05 11:57:39 -0700

    Add helloworld

---
+++ helloworld/HelloPrinter.java
@@ -0,0 +1,13 @@
+/*
+    Filename: math.cpp
+    Problem: Sample Problem
+    Description: Comple and play with!
+    Author: Joshua Rodriguez
+    Email: ewized@gmail.com
+    Date: 2/4/2014
+*/
+public class HelloPrinter {
+    public static void main(String args[]) {
+        System.out.println("Hello World");   
+    }
+}
